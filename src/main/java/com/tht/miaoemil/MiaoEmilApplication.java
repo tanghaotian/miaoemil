d809a78044a72e17279cfad99a57ae34e9c42cdf
tht <545804513@qq.com>
2022-03-29 11:00:08 +0800

    package structure

---
+++ src/main/java/com/tht/miaoemil/MiaoEmilApplication.java
@@ -0,0 +1,14 @@
+package com.tht.miaoemil;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@SpringBootConfiguration
+@EnableScheduling
+public class MiaoEmilApplication {
+    public static void main(String[] args) {
+
+    }
+}
